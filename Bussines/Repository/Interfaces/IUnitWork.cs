61e70686ea0c1e257a82447e94c58a0579a98edb
fcmorenob <fabis_Sk8@hotmail.com>
2025-08-14 22:31:20 -0500

    :constructor: [FM]  configuracion Unidad de trabajo

--- Bussines/Repository/Interfaces/IUnitWork.cs
+++ Bussines/Repository/Interfaces/IUnitWork.cs
@@ -8,7 +8,20 @@ namespace Bussines.Repository.Interfaces
 {
     public interface IUnitWork:IDisposable
     {
-        
+        IClienteRepository ClienteRepository { get; }
+        IConductorRepository ConductorRepository { get; }
+        IEstadoParadaRepository EstadoParadaRepository { get; }
+        IEstadoPedidoRepository EstadoPedidoRepository { get; }
+        IEstadoRutaRepository EstadoRutaRepository { get; }
+        IhistorialEstadoPedidoRepository HistorialEstadoPedidoRepository { get; }
+        IParadaEntregaRepository ParadaEntregaRepository { get; }
+        IPedidoDetalleRepository PedidoDetalleRepository { get; }
+        IPedidoRepository PedidoRepository { get; }
+        IRutaEntregaRepository RutaEntregaRepository { get; }
+        ITiendaRepository TiendaRepository { get; }
+        IVehiculoRepository VehiculoRepository { get; }
+        IVwResumenEntregaDiasRepository VwResumenEntregaDiasRepository { get; }
+
         void Save();
     }
 }
--- Bussines/Repository/UnitWork.cs
+++ Bussines/Repository/UnitWork.cs
@@ -13,8 +13,47 @@ namespace Bussines.Repository
         public UnitWork(DeliveryContext _context)
         {
             context = _context;
-           
+            ClienteRepository = new ClienteRepository(context);
+            ConductorRepository = new ConductorRepository(context);
+            EstadoParadaRepository = new EstadoParadaRepository(context);
+            EstadoPedidoRepository = new EstadoPedidoRepository(context);
+            EstadoRutaRepository = new EstadoRutaRepository(context);
+            HistorialEstadoPedidoRepository = new HistorialEstadoPedidoRepository(context);
+            ParadaEntregaRepository = new ParadaEntregaRepository(context);
+            PedidoDetalleRepository = new PedidoDetalleRepository(context);
+            PedidoRepository = new PedidoRepository(context);
+            RutaEntregaRepository = new RutaEntregaRepository(context);
+            TiendaRepository = new TiendaRepository(context);
+            VehiculoRepository = new VehiculoRepository(context);
+            VwResumenEntregaDiasRepository = new VwResumenEntregaDiasRepository(context);
+
+
         }
+
+        public IClienteRepository ClienteRepository { get; set; }
+
+        public IConductorRepository ConductorRepository { get; set; }
+
+        public IEstadoParadaRepository EstadoParadaRepository { get; set; }
+
+        public IEstadoPedidoRepository EstadoPedidoRepository { get; set; }
+
+        public IEstadoRutaRepository EstadoRutaRepository { get; set; }
+
+        public IhistorialEstadoPedidoRepository HistorialEstadoPedidoRepository { get; set; }
+
+        public IParadaEntregaRepository ParadaEntregaRepository { get; set; }
+        public IPedidoDetalleRepository PedidoDetalleRepository { get; set; }
+
+        public IPedidoRepository PedidoRepository { get; set; }
+
+        public IRutaEntregaRepository RutaEntregaRepository { get; set; }
+
+        public ITiendaRepository TiendaRepository { get; set; }
+
+        public IVehiculoRepository VehiculoRepository { get; set; }
+
+        public IVwResumenEntregaDiasRepository VwResumenEntregaDiasRepository { get; set; }
 
         public void Dispose()
         {
